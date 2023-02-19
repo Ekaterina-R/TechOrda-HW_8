c31f5c3b92f36d04eae9b69bb4b2c825f1949813
Ekaterina Romanenko <kat.romanenko24@gmail.com>
2023-02-20 01:30:22 +0600

    Посты сделаны, остались комментарии

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
+interface POST {
+  id: number;
+  title: string;
+  body: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'hw_8';
+  data: POST[] = [];
+
+  constructor() {
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        this.data = json;
+      });
+  }
 }
